7d0798abe64d9819c313c83a2650f4aab163cbc1
Mees <77725857+MeesJ@users.noreply.github.com>
2021-02-15 22:52:02 +0100

    Fixed typo in instructions
    There wasn't a typo in the code itselves, no worries!

--- CoreGui/DisableEmotesWheel.lua
+++ CoreGui/DisableEmotesWheel.lua
@@ -4,5 +4,5 @@ This script disables the R15 emotes wheel (and makes it unable to get opened).
 To make this script work, paste the code in a LocalScript and move it into StarterGui.
 --]]
 
-wait(5) -- I'd strongly recommend using a wait function. Your players won't notice this, but it strongly decreases the chances that the script will fail to work properly.
+wait(5) -- I'd strongly recommend using a wait function. Your players won't notice this, but it strongly decreases the chances that the script will fail to work.
 game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.EmotesMenu, false)
--- CoreGui/DisableResetButton.lua
+++ CoreGui/DisableResetButton.lua
@@ -4,5 +4,5 @@ This script greys out the reset button in the game menu and makes it unable to g
 To make this script work, paste the code in a LocalScript and move it into StarterGui.
 --]]
 
-wait(5) -- I'd strongly recommend using a wait function. Your players won't notice this, but it strongly decreases the chances that the script will fail to work properly.
+wait(5) -- I'd strongly recommend using a wait function. Your players won't notice this, but it strongly decreases the chances that the script will fail to work.
 game:GetService("StarterGui"):SetCore("ResetButtonCallback", false)

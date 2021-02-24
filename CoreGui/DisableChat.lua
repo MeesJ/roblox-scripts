6431792c7548da741ac91a3f6e8eaca394abcb25
Mees <77725857+MeesJ@users.noreply.github.com>
2021-02-24 12:41:22 +0100

    Usage instructions added

--- CoreGui/DisableChat.lua
+++ CoreGui/DisableChat.lua
@@ -1,2 +1,8 @@
+--[[
+This script removes the in-game chat. Please note that this script only changes the front-end. To actually disable the chat scripts from loading, set Chat.LoadDefaultChat to false (https://i.imgur.com/pCKGGyx.gif).
+
+To make this script work, paste the code in a LocalScript and move it into StarterGui.
+--]]
+
 wait(5)
 game.StarterGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)

4cf91bd09b9ea3c5c05950e787b47e189fce44c8
Mees <77725857+MeesJ@users.noreply.github.com>
2021-03-04 19:34:06 +0100

    Usage instructions added

--- Tool Giver on Click/ToolGiver.lua
+++ Tool Giver on Click/ToolGiver.lua
@@ -1,4 +1,10 @@
-local tool = game.ServerStorage.GreenBrew
+--[[
+Put this script directly inside of the part you want the player has to click to get the tool you want them to get. The part which has to be clicked must contain a ClickDetector (don't change the ClickDetector name).
+
+The tool you want to be given on click must be placed in ServerStorage. The name can't contain special characters or spaces!
+--]]
+
+local tool = game.ServerStorage.TOOLNAME -- Replace TOOLNAME with the exact name of the tool you want to be given
 
 script.Parent.ClickDetector.MouseClick:connect(function(player)
 	local Backpack = player:WaitForChild("Backpack")

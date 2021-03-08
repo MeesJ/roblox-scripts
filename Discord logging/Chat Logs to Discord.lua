782fc4e292711be6a4b11020521c1a98ca4ed2d0
Mees <77725857+MeesJ@users.noreply.github.com>
2021-03-08 19:09:47 +0100

    Changed reference to ThumbnailAPI.md

--- Discord logging/Chat Logs to Discord.lua
+++ Discord logging/Chat Logs to Discord.lua
@@ -11,7 +11,7 @@ Players.PlayerAdded:Connect(function(plr)
 		local data = {
 			content = "`"..msg.."`"; -- Sends the message the player sent as message in Discord. I've put those two backticks to
 			username = plr.Name.." ("..plr.UserId..")"; -- In this case, we are using the username and user ID between the brackets as sender name to appear in Discord. Having the user ID included is not required, but I strongly advise you to keep it in to be compliant with GDPR (as it will make it way much more easy to take action if you get a Right to Erasure message from Roblox)
-			avatar_url = "https://www.roblox.com/headshot-thumbnail/image?userId="..plr.UserId.."&width=250&height=250&format=png" -- Uses the headshot thumbnail of the player who sent the message as sender avatar in Discord. Please check the README.md file in /roblox-scripts to see other thumbnails you can use (for example, full avatar or bust).
+			avatar_url = "https://www.roblox.com/headshot-thumbnail/image?userId="..plr.UserId.."&width=250&height=250&format=png" -- Uses the headshot thumbnail of the player who sent the message as sender avatar in Discord. Please check the ThumbnailAPI.md file in /roblox-scripts to see other thumbnails you can use (for example, full avatar or bust).
 		}
 		HttpService:PostAsync(webhook, HttpService:JSONEncode(data))
 	end)

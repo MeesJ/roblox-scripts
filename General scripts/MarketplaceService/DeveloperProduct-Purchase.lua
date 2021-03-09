53cb84bbceaedfb8b40c08630489c21329164d3e
Mees <77725857+MeesJ@users.noreply.github.com>
2021-03-09 12:14:01 +0100

    Added note

--- General scripts/MarketplaceService/DeveloperProduct-Purchase.lua
+++ General scripts/MarketplaceService/DeveloperProduct-Purchase.lua
@@ -2,6 +2,8 @@
 This script prompts a Developer Product purchase to the player who clicked the part where this script is located in.
 Make sure to insert a ClickDetector in the part you want to be clicked to prompt the purchase.
 
+Important to know is that this is a very simple script. It currently only prompts the purchase, that's all. It doesn't do anything else.
+
 Unlike Game Passes which are permanent and an one-time purchase, Developer Products can be bought over and over again.
 For more information about Developer Products, check the article below on the Developer Hub:
 https://developer.roblox.com/en-us/articles/Developer-Products-In-Game-Purchases

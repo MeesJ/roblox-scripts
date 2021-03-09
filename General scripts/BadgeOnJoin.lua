1ed5be23658b0768aa29514ab7faea62517e56f1
Mees <77725857+MeesJ@users.noreply.github.com>
2021-03-09 12:12:12 +0100

    Usage instructions added
    I forget to add those when I added the script, sorry.

--- General scripts/BadgeOnJoin.lua
+++ General scripts/BadgeOnJoin.lua
@@ -1,9 +1,15 @@
+--[[
+This script gives the badge you've choosen to all players who join your game.
+
+I'd recommend you to place this script in ServerScriptService, but it usually doesn't affect the working of the script if you place it somewhere else.
+--]]
+
 local BadgeService = game:GetService("BadgeService")
-local BadgeID = 2124697671
+local BadgeID = 12345678 -- Replace this with the ID of the badge you want to be given on join
 
 game:GetService("Players").PlayerAdded:Connect(function(player)
 	wait(5)
-	if not BadgeService:UserHasBadgeAsync(player.UserId, BadgeID) then
+	if not BadgeService:UserHasBadgeAsync(player.UserId, BadgeID) then -- To prevent unnecessary errors in the console, we first check if the player doesn't have the badge yet
 		BadgeService:AwardBadge(player.UserId, BadgeID)
 	end
 end)

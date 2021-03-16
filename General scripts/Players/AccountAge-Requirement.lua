cb097beef340ec676ef142b94f76ca369c360fa5
Mees <77725857+MeesJ@users.noreply.github.com>
2021-03-16 09:31:16 +0100

    Added clearer instructions

--- General scripts/Players/AccountAge-Requirement.lua
+++ General scripts/Players/AccountAge-Requirement.lua
@@ -1,3 +1,9 @@
+--[[
+This script allows you to set an age requirement for players who join your game. This is useful for prevent people creating alts to circumvent bans.
+
+I'd recommend you to place this script in ServerScriptService, but it usually doesn't affect the working of the script if you place it somewhere else.
+--]]
+
 local AgeRequirement = 10 -- Replace 10 with the amount of days Roblox accounts should be to join your game
 
 game.Players.PlayerAdded:Connect(function(player)

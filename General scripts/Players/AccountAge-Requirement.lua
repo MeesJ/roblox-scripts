736826443c1530630f37bd979e954f53f4f48ebb
Mees <77725857+MeesJ@users.noreply.github.com>
2021-03-16 09:19:06 +0100

    two variables used
    This broke the working of the script. Please be aware, only scripts in the published (main) branch are tested and confirmed working!

--- General scripts/Players/AccountAge-Requirement.lua
+++ General scripts/Players/AccountAge-Requirement.lua
@@ -2,6 +2,6 @@ local AgeRequirement = 10 -- Replace 10 with the amount of days Roblox accounts 
 
 game.Players.PlayerAdded:Connect(function(player)
 	if player.AccountAge < AgeRequirement then
-		plr:Kick("Your account is too young to join this game. Please come back later!") -- The message players under the age requirement see when trying to join
+		player:Kick("Your account is too young to join this game. Please come back later!") -- The message players under the age requirement see when trying to join
 	end
 end)

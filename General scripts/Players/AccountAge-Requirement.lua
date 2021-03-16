6dffc874949e5da5a1213ce704dbaf9af2e94021
Mees <77725857+MeesJ@users.noreply.github.com>
2021-03-16 09:17:18 +0100

    Finished script

--- General scripts/Players/AccountAge-Requirement.lua
+++ General scripts/Players/AccountAge-Requirement.lua
@@ -1,1 +1,7 @@
--- soon
+local AgeRequirement = 10 -- Replace 10 with the amount of days Roblox accounts should be to join your game
+
+game.Players.PlayerAdded:Connect(function(player)
+	if player.AccountAge < AgeRequirement then
+		plr:Kick("Your account is too young to join this game. Please come back later!") -- The message players under the age requirement see when trying to join
+	end
+end)
